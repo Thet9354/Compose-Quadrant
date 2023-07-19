bb7b5d4b8de83c0aafcf44a635829e20776517a5
PhoonThetPine <thetpine254@gmail.com>
2023-07-19 20:39:16 +0800

    Android Kotlin Course Unit 1 Compose Quadrant - V5

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.composequadrant
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
@@ -23,6 +30,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,19 +68,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeQuadrantApp() {
+    val fadeAnim = fadeInAnimation()
+
     Column(Modifier.fillMaxWidth()) {
         Row(Modifier.weight(1f)) {
             ComposableInfoCard(
                 title = stringResource(R.string.first_title),
                 description = stringResource(R.string.first_description),
                 backgroundColor = Color(0xFFEADDFF),
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .alpha(fadeAnim[0].value)
             )
             ComposableInfoCard(
                 title = stringResource(R.string.second_title),
                 description = stringResource(R.string.second_description),
                 backgroundColor = Color(0xFFD0BCFF),
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .alpha(fadeAnim[1].value)
             )
         }
         Row(Modifier.weight(1f)) {
@@ -79,16 +94,46 @@ fun ComposeQuadrantApp() {
                 title = stringResource(R.string.third_title),
                 description = stringResource(R.string.third_description),
                 backgroundColor = Color(0xFFB69DF8),
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .alpha(fadeAnim[2].value)
             )
             ComposableInfoCard(
                 title = stringResource(R.string.fourth_title),
                 description = stringResource(R.string.fourth_description),
                 backgroundColor = Color(0xFFF6EDFF),
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .alpha(fadeAnim[3].value)
             )
         }
     }
+}
+
+@Composable
+fun fadeInAnimation(): List<Animatable<Float, AnimationVector1D>> {
+
+    Log.d("ComposeQuadrant", "Before launching the animation")
+    val animationSpec = tween<Float>(durationMillis = 500)
+
+    return remember {
+        List(4) {
+            Animatable(0f)
+        }
+    }.also { alphaList ->
+        LaunchedEffect(Unit) {
+            alphaList.forEachIndexed { index, alphaAnim ->
+                Log.d("ComposeQuadrant", "Animating card at index: $index")
+                launch {
+                    delay(index * 200L)
+                    alphaAnim.animateTo(1f, animationSpec = animationSpec)
+                }
+            }
+        }
+    }
+
+    Log.d("ComposeQuadrant", "After launching the animation")
+
 }
 
 @Composable
@@ -127,3 +172,4 @@ fun DefaultPreview() {
         ComposeQuadrantApp()
     }
 }
+
